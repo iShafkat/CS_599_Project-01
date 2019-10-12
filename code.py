18f720511b2bbb9aefe13a184e23dbc1677f50af
Shafkat Islam <si229@nau.edu>
2019-10-11 20:33:36 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,39 +1,63 @@
-from __future__ import print_function, division
-import os
-import torch
-import pandas as pd
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-# Read the Dataset
-train_array = np.loadtxt("zip.train")
-train_data = pd.DataFrame(train_array)
-test_array = np.loadtxt("zip.test")
-test_data = pd.DataFrame(test_array)
-#Neural Network Architecture
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+input_size = 784       # The image size = 28 x 28 = 784
+hidden_size = 500      # The number of nodes at the hidden layer
+num_classes = 10       # The number of output classes. In this case, from 0 to 9
+num_epochs = 5         # The number of times entire dataset is trained
+batch_size = 100       # The size of input data took for one iteration
+learning_rate = 0.001  # The speed of convergence
+train_dataset = dsets.MNIST(root='./data',
+                           train=True,
+                           transform=transforms.ToTensor(),
+                           download=True)
+
+test_dataset = dsets.MNIST(root='./data',
+                           train=False,
+                           transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
 class Net(nn.Module):
-
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()                    # Inherited from the parent class nn.Module
+        self.fc1 = nn.Linear(input_size, hidden_size)  # 1st Full-Connected Layer: 784 (input data) -> 500 (hidden node)
+        self.relu = nn.ReLU()                          # Non-Linear ReLU Layer: max(0,x)
+        self.fc2 = nn.Linear(hidden_size, num_classes) # 2nd Full-Connected Layer: 500 (hidden node) -> 10 (output class)
+    
+    def forward(self, x):                              # Forward pass: stacking each layer together
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+ net = Net(input_size, hidden_size, num_classes)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):   # Load a batch of images with its (index, data, class)
+        images = Variable(images.view(-1, 28*28))         # Convert torch tensor to Variable: change image from a vector of size 784 to a matrix of 28 x 28
+        labels = Variable(labels)
+        
+        optimizer.zero_grad()                             # Intialize the hidden weight to all zeros
+        outputs = net(images)                             # Forward pass: compute the output class given a image
+        loss = criterion(outputs, labels)                 # Compute the loss: difference between the output class and the pre-given label
+        loss.backward()                                   # Backward pass: compute the weight
+        optimizer.step()                                  # Optimizer: update the weights of hidden nodes
+        
+        
+correct = 0
+total = 0
+for images, labels in test_loader:
+    images = Variable(images.view(-1, 28*28))
+    outputs = net(images)
+    _, predicted = torch.max(outputs.data, 1)  # Choose the best class from the output: The class with the best score
+    total += labels.size(0)                    # Increment the total count
+    correct += (predicted == labels).sum()     # Increment the correct count
+    
+print('Accuracy of the network on the 10K test images: %d %%' % (100 * correct / total))
