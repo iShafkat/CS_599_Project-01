55447671d1f022607a8c0acf6ac2c52c9e21ab3e
Shafkat Islam <si229@nau.edu>
2019-10-17 11:54:11 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,63 +1,120 @@
-import torch
-import torch.nn as nn
-import torchvision.datasets as dsets
-import torchvision.transforms as transforms
-from torch.autograd import Variable
-input_size = 784       # The image size = 28 x 28 = 784
-hidden_size = 500      # The number of nodes at the hidden layer
-num_classes = 10       # The number of output classes. In this case, from 0 to 9
-num_epochs = 5         # The number of times entire dataset is trained
-batch_size = 100       # The size of input data took for one iteration
-learning_rate = 0.001  # The speed of convergence
-train_dataset = dsets.MNIST(root='./data',
-                           train=True,
-                           transform=transforms.ToTensor(),
-                           download=True)
-
-test_dataset = dsets.MNIST(root='./data',
-                           train=False,
-                           transform=transforms.ToTensor())
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                          batch_size=batch_size,
-                                          shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size,
-                                          shuffle=False)
-class Net(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(Net, self).__init__()                    # Inherited from the parent class nn.Module
-        self.fc1 = nn.Linear(input_size, hidden_size)  # 1st Full-Connected Layer: 784 (input data) -> 500 (hidden node)
-        self.relu = nn.ReLU()                          # Non-Linear ReLU Layer: max(0,x)
-        self.fc2 = nn.Linear(hidden_size, num_classes) # 2nd Full-Connected Layer: 500 (hidden node) -> 10 (output class)
-    
-    def forward(self, x):                              # Forward pass: stacking each layer together
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
- net = Net(input_size, hidden_size, num_classes)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):   # Load a batch of images with its (index, data, class)
-        images = Variable(images.view(-1, 28*28))         # Convert torch tensor to Variable: change image from a vector of size 784 to a matrix of 28 x 28
-        labels = Variable(labels)
-        
-        optimizer.zero_grad()                             # Intialize the hidden weight to all zeros
-        outputs = net(images)                             # Forward pass: compute the output class given a image
-        loss = criterion(outputs, labels)                 # Compute the loss: difference between the output class and the pre-given label
-        loss.backward()                                   # Backward pass: compute the weight
-        optimizer.step()                                  # Optimizer: update the weights of hidden nodes
-        
-        
-correct = 0
-total = 0
-for images, labels in test_loader:
-    images = Variable(images.view(-1, 28*28))
-    outputs = net(images)
-    _, predicted = torch.max(outputs.data, 1)  # Choose the best class from the output: The class with the best score
-    total += labels.size(0)                    # Increment the total count
-    correct += (predicted == labels).sum()     # Increment the correct count
-    
-print('Accuracy of the network on the 10K test images: %d %%' % (100 * correct / total))
+#import libraries
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.utils import to_categorical
+from keras import losses
+from keras.layers import LocallyConnected1D, LocallyConnected2D, Conv2D
+import matplotlib.pyplot as plt
+import numpy as np
+def main():
+    # Load the Training Dataset
+    dataset = loadtxt('zip.train')  
+    X = dataset[0:500,1:257]
+    y = dataset[0:500,0:1]
+    y = to_categorical(y)
+    #load the Test Dataset
+    test_array = loadtxt("zip.test")
+    test_label1 = test_array[0:250,0:1]
+    test_array = test_array[0:250,1:257]
+    test_label = to_categorical(test_label1)
+    accuracy_one =network_one(X,y,test_label1,test_array)
+    accuracy_two =network_two(X,y,test_label1,test_array)
+    accuracy_three =network_three(X,y,test_label1,test_array)
+    accuracy_four =network_four(X,y,test_label1,test_array)
+    t = np.arange(0., 30., 1)
+    plt.plot(t, accuracy_one, 'r', t, accuracy_two,'b', t, accuracy_three, 'g',t, accuracy_four, 'y')
+   # plt.plot(t, accuracy_four, 'r')
+    plt.xlabel("Training Epochs")
+    plt.ylabel("%Correct On Test Data")
+    plt.show()
+    plt.savefig('cs599_output.png')
+ def network_one(X,y,test_label1,test_array):
+    accuracy_one, epoch =[],1
+    model = Sequential()
+    model.add(Dense(10, input_dim=256, activation='sigmoid'))
+    model.compile(loss=losses.mean_squared_error, optimizer='adam', metrics=['accuracy'])
+    while epoch<31:
+        model.fit(X, y, epochs=epoch, batch_size=10)
+        _, accuracy = model.evaluate(X, y)
+        predictions = model.predict_classes(test_array)
+        correct =0
+        for i in range(len(predictions)):
+            if predictions[i]==test_label1[i]:
+                correct +=1
+            else:
+                pass
+        accuracy =100 * correct/len(predictions)
+        accuracy_one.append(accuracy)
+        epoch +=1
+    return accuracy_one
+  def network_two(X,y,test_label1,test_array):
+    accuracy_two, epoch =[],1
+    model = Sequential()
+    model.add(Dense(12, input_dim=256, activation='relu'))
+    model.add(Dense(10, activation='sigmoid'))
+    model.compile(loss=losses.mean_squared_error, optimizer='adam', metrics=['accuracy'])
+    while epoch<31:
+        model.fit(X, y, epochs=epoch, batch_size=10)
+        _, accuracy = model.evaluate(X, y)
+        predictions = model.predict_classes(test_array)
+        correct =0
+        for i in range(len(predictions)):
+            if predictions[i]==test_label1[i]:
+                correct +=1
+            else:
+                pass
+        accuracy =100 * correct/len(predictions)
+        accuracy_two.append(accuracy)
+        epoch +=1
+    return accuracy_two
+  def network_three(X,y,test_label1,test_array):
+    accuracy_three, epoch =[],1
+    model = Sequential()
+    X= X.reshape(X.shape[0], 16, 16,1).astype('float32')
+    test_array= test_array.reshape(test_array.shape[0], 16, 16,1).astype('float32')
+    model.add(LocallyConnected2D(64, (3,3), input_shape=(16, 16,1), activation='relu'))
+    model.add(LocallyConnected2D(16, (5,5), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(10, activation='sigmoid'))
+    model.compile(loss=losses.mean_squared_error, optimizer='adam', metrics=['accuracy'])
+    while epoch<31:
+        model.fit(X, y, epochs=epoch, batch_size=10)
+        _, accuracy = model.evaluate(X, y)
+        predictions = model.predict_classes(test_array)
+        correct =0
+        for i in range(len(predictions)):
+            if predictions[i]==test_label1[i]:
+                correct +=1
+            else:
+                pass
+        accuracy =100 * correct/len(predictions)
+        accuracy_three.append(accuracy)
+        epoch +=1
+    return accuracy_three
+  def network_four(X,y,test_label1,test_array):
+    accuracy_four, epoch =[],1
+    model = Sequential()
+    X= X.reshape(X.shape[0], 16, 16,1).astype('float32')
+    test_array= test_array.reshape(test_array.shape[0], 16, 16,1).astype('float32')
+    model.add(Conv2D(128,(3,3), input_shape=(16,16,1), activation='relu'))
+    model.add(LocallyConnected2D(16, (5,5), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(10, activation='sigmoid'))
+    model.compile(loss=losses.mean_squared_error, optimizer='adam', metrics=['accuracy'])
+    while epoch<31:
+        model.fit(X, y, epochs=epoch, batch_size=10)
+        _, accuracy = model.evaluate(X, y)
+        predictions = model.predict_classes(test_array)
+        correct =0
+        for i in range(len(predictions)):
+            if predictions[i]==test_label1[i]:
+                correct +=1
+            else:
+                pass
+        accuracy =100 * correct/len(predictions)
+        accuracy_four.append(accuracy)
+        epoch +=1
+    return accuracy_four
+  if __name__ == '__main__':
+    main()
